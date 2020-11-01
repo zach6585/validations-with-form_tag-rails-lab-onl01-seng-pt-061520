74fed9e79169ca4364c4985dd6124bf91dfcac71
zach6585 <zmines@wesleyan.edu>
2020-11-01 13:43:35 -0500

    commit tree 6cbdbe775d13e535deb79d6a688b63b63c2b8f78

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: [:show, :new, :edit, :create, :update]
+  resources :posts, only: [:show, :new, :edit, :create, :update]
 end
